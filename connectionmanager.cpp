e019f8ad06aa4f65b9e1d723438bfd10cb8132a9
StasGordynets <stasgordynets@gmail.com>
2016-05-13 09:46:20 +0300

    Add new files for course work
    Add new files for course work

---
+++ connectionmanager.cpp
@@ -0,0 +1,49 @@
+
+#include "connectionmanager.h"
+
+#include <QByteArray>
+#include <QDateTime>
+
+static const int MaxConnections = 250;
+
+Q_GLOBAL_STATIC(ConnectionManager, connectionManager)
+
+ConnectionManager *ConnectionManager::instance()
+{
+    return connectionManager();
+}
+
+bool ConnectionManager::canAddConnection() const
+{
+    return (connections.size() < MaxConnections);
+}
+
+void ConnectionManager::addConnection(PeerWireClient *client)
+{
+    connections << client;
+}
+
+void ConnectionManager::removeConnection(PeerWireClient *client)
+{
+    connections.remove(client);
+}
+
+int ConnectionManager::maxConnections() const
+{
+    return MaxConnections;
+}
+
+QByteArray ConnectionManager::clientId() const
+{
+    if (id.isEmpty()) {
+        // Generate peer id
+        int startupTime = int(QDateTime::currentDateTime().toTime_t());
+
+        QString s;
+        s.sprintf("-QT%04x-", (QT_VERSION % 0xffff00) >> 8);
+        id += s.toLatin1();
+        id += QByteArray::number(startupTime, 10);
+        id += QByteArray(20 - id.size(), '-');
+    }
+    return id;
+}
